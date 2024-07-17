ebdec981689c926d21c281f5de4d13c2a3df0b70
piyushvermaa <piyushsoniiii.07@gmail.com>
2024-07-18 01:54:11 +0530

    style fix

--- src/components/SignupPage.tsx
+++ src/components/SignupPage.tsx
@@ -40,7 +40,7 @@ const SignupCard: React.FC<SignupCardProps> = ({ onLoginClick }) => {
   };
 
   return (
-    <div className="flex items-center gap-10 justify-center bg-black">
+    <div className="flex items-center gap-10 justify-center bg-black p-10">
       <div className="hidden md:block">
         {/* Use next/image for Signuppage */}
         <Image src="/Signuppage.png" alt="Signup Page Image" width={600} height={600} className="" />
